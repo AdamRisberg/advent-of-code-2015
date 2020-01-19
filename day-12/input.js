0e2061fc9ecf106bb3c5ce4bc0be7c22a8a56de8
Adam Risberg <myniztan@hotmail.com>
2020-01-19 00:00:54 -0600

    Add day 12 puzzle inputs

--- day-12/input.js
+++ day-12/input.js
@@ -1,9 +1,8 @@
 const path = require("path");
 const fs = require("fs");
 
-const input = fs.readFileSync(
-  path.resolve(__dirname, "raw-input.txt"),
-  "UTF-8"
+const input = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, "raw-input.txt"), "UTF-8")
 );
 
 module.exports = input;
