d4142f7b1c36a3cb7529b40660858a837d9dc0cf
Adam Risberg <myniztan@hotmail.com>
2020-01-22 04:18:04 -0600

    Add day 19 puzzle inputs

--- day-19/input.js
+++ day-19/input.js
@@ -3,6 +3,15 @@ const fs = require("fs");
 
 const input = fs
   .readFileSync(path.resolve(__dirname, "raw-input.txt"), "UTF-8")
-  .split("\r\n");
+  .split("\r\n")
+  .filter(Boolean);
 
-module.exports = input;
+const replacements = input
+  .slice(0, input.length - 1)
+  .map(item => item.split(" => "));
+const molecule = input[input.length - 1];
+
+module.exports = {
+  replacements,
+  molecule
+};
