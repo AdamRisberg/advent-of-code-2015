8009240b6e07662ba254f60e91d6b1ed838346e6
Adam Risberg <myniztan@hotmail.com>
2020-01-23 14:15:05 -0600

    Add day 21 puzzle inputs

--- day-21/input.js
+++ day-21/input.js
@@ -1,8 +1,140 @@
-const path = require("path");
-const fs = require("fs");
+const bossStats = {
+  hitPoints: 100,
+  damage: 8,
+  armor: 2
+};
 
-const input = fs
-  .readFileSync(path.resolve(__dirname, "raw-input.txt"), "UTF-8")
-  .split("\r\n");
+const playerStats = {
+  hitPoints: 100,
+  damage: 0,
+  armor: 0
+};
 
-module.exports = input;
+const store = {
+  weapons: [
+    {
+      name: "Dagger",
+      cost: 8,
+      damage: 4,
+      armor: 0
+    },
+    {
+      name: "Shortsword",
+      cost: 10,
+      damage: 5,
+      armor: 0
+    },
+    {
+      name: "Warhammer",
+      cost: 25,
+      damage: 6,
+      armor: 0
+    },
+    {
+      name: "Longsword",
+      cost: 40,
+      damage: 7,
+      armor: 0
+    },
+    {
+      name: "Greataxe",
+      cost: 74,
+      damage: 8,
+      armor: 0
+    }
+  ],
+  armor: [
+    {
+      name: "none",
+      cost: 0,
+      damage: 0,
+      armor: 0
+    },
+    {
+      name: "Leather",
+      cost: 13,
+      damage: 0,
+      armor: 1
+    },
+    {
+      name: "Chainmail",
+      cost: 31,
+      damage: 0,
+      armor: 2
+    },
+    {
+      name: "Splintmail",
+      cost: 53,
+      damage: 0,
+      armor: 3
+    },
+    {
+      name: "Bandedmail",
+      cost: 75,
+      damage: 0,
+      armor: 4
+    },
+    {
+      name: "Platemail",
+      cost: 102,
+      damage: 0,
+      armor: 5
+    }
+  ],
+  rings: [
+    {
+      name: "none",
+      cost: 0,
+      damage: 0,
+      armor: 0
+    },
+    {
+      name: "Damage +1",
+      cost: 25,
+      damage: 1,
+      armor: 0
+    },
+    {
+      name: "Damage +2",
+      cost: 50,
+      damage: 2,
+      armor: 0
+    },
+    {
+      name: "Damage +3",
+      cost: 100,
+      damage: 3,
+      armor: 0
+    },
+    {
+      name: "none",
+      cost: 0,
+      damage: 0,
+      armor: 0
+    },
+    {
+      name: "Defense +1",
+      cost: 20,
+      damage: 0,
+      armor: 1
+    },
+    {
+      name: "Defense +2",
+      cost: 40,
+      damage: 0,
+      armor: 2
+    },
+    {
+      name: "Defense +3",
+      cost: 80,
+      damage: 0,
+      armor: 3
+    }
+  ]
+};
+
+module.exports = {
+  bossStats,
+  playerStats,
+  store
+};
