ca098fdddbd27110934b461b3e8737f506d40fd2
Adam Risberg <myniztan@hotmail.com>
2020-01-23 23:59:09 -0600

    Finish day 23 solution

--- day-23/solution.js
+++ day-23/solution.js
@@ -1,1 +1,13 @@
 const input = require("./input");
+const Computer = require("./computer");
+
+const computer = new Computer();
+computer.loadProgram(input);
+computer.run();
+
+console.log(`Part 1: ${computer.b}`);
+
+computer.loadProgram(input, 1);
+computer.run();
+
+console.log(`Part 2: ${computer.b}`);
