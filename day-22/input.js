5965afc74e456b69994c66e45461d52e05a42857
Adam Risberg <myniztan@hotmail.com>
2020-01-23 20:19:45 -0600

    Add day-22 puzzle inputs

--- day-22/input.js
+++ day-22/input.js
@@ -1,8 +1,68 @@
-const path = require("path");
-const fs = require("fs");
+const spells = [
+  {
+    name: "Magic Missile",
+    cost: 53,
+    damage: 4,
+    heal: 0,
+    armor: 0,
+    mana: 0,
+    instant: true,
+    lasts: 0
+  },
+  {
+    name: "Drain",
+    cost: 73,
+    damage: 2,
+    heal: 2,
+    armor: 0,
+    mana: 0,
+    instant: true,
+    lasts: 0
+  },
+  {
+    name: "Shield",
+    cost: 113,
+    damage: 0,
+    heal: 0,
+    armor: 7,
+    mana: 0,
+    instant: false,
+    lasts: 6
+  },
+  {
+    name: "Poison",
+    cost: 173,
+    damage: 3,
+    heal: 0,
+    armor: 0,
+    mana: 0,
+    instant: false,
+    lasts: 6
+  },
+  {
+    name: "Recharge",
+    cost: 229,
+    damage: 0,
+    heal: 0,
+    armor: 0,
+    mana: 101,
+    instant: false,
+    lasts: 5
+  }
+];
 
-const input = fs
-  .readFileSync(path.resolve(__dirname, "raw-input.txt"), "UTF-8")
-  .split("\r\n");
+const playerStats = {
+  health: 50,
+  mana: 500
+};
 
-module.exports = input;
+const bossStats = {
+  health: 51,
+  damage: 9
+};
+
+module.exports = {
+  spells,
+  playerStats,
+  bossStats
+};
